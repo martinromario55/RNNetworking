2e6a91c61d44d72d5fa76aacf440dd23205dc0b2
martinromario55 <martinromario55@gmail.com>
2024-02-03 14:10:30 +0300

    Fetching Data from Jsonplaceholder

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as Status } from 'expo-status-bar'
+import { useEffect, useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+} from 'react-native'
 
 export default function App() {
+  // Fetch data from jsonplaceholder function
+  const [posts, setPosts] = useState([])
+
+  const fetchData = async (limit = 10) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+    )
+    const data = await response.json()
+    // console.log(data)
+    setPosts(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <Status style="auto" />
+      <View style={styles.listcontainer}>
+        <FlatList
+          data={posts}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.titleText}>{item.title}</Text>
+              <Text style={styles.bodyText}>{item.body}</Text>
+            </View>
+          )}
+          ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+          ListEmptyComponent={<Text>No Posts Found</Text>}
+          ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
+          ListFooterComponent={
+            <Text style={styles.footerText}>End of List</Text>
+          }
+        />
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight,
   },
-});
+  listcontainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 16,
+    marginVertical: 8,
+    borderRadius: 8,
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  bodyText: {
+    fontSize: 16,
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 12,
+  },
+  footerText: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginTop: 12,
+  },
+})
